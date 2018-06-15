78f306a819af7460cdaecef159e0c0310d59c6c9
Burak Tilek <burak.tilek@gmail.com>
2018-06-15 17:32:35 +0300

    Alt attributes implemeted inside fillRestaurantHTML function

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -87,7 +87,8 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   address.innerHTML = restaurant.address;
 
   const image = document.getElementById('restaurant-img');
-  image.className = 'restaurant-img'
+  image.className = 'restaurant-img';
+  image.setAttribute('alt', `Picture of restaurant ${restaurant.name}`);
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
