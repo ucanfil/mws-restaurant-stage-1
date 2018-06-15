c2a86992dd998b648e7801c060ba1a4aae8d7c41
Burak Tilek <burak.tilek@gmail.com>
2018-06-15 17:29:42 +0300

    Alt attributes implemented in createRestaurantHTML function

--- js/main.js
+++ js/main.js
@@ -158,6 +158,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.setAttribute('alt', `Picture of restaurant ${restaurant.name}`);
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
